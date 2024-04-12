eef18de4958848344a4ff05eed5bef8b7d415f95
VoVanKhoa <vankhoa.work@gmail.com>
2024-04-12 18:17:11 +0700

    feat/emptw19-create

--- src/pages/Project/CreateProject/CreateProject.jsx
+++ src/pages/Project/CreateProject/CreateProject.jsx
@@ -1,124 +1,167 @@
-import React from 'react';
-import {Form, Input, Select, Row, Col, DatePicker, Button, Divider, Typography, Breadcrumb} from 'antd';
-import {PlusOutlined} from '@ant-design/icons';
-import {useTranslation} from 'react-i18next';
-import {useNavigate} from "react-router-dom";
+import React from "react";
+import {
+    Form,
+    Input,
+    Select,
+    Row,
+    Col,
+    DatePicker,
+    Button,
+    Divider,
+    Typography,
+    Breadcrumb,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
-const {Option} = Select;
+const { Option } = Select;
 
 const CreateProject = () => {
     const navigate = useNavigate();
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     const onFinish = (values) => {
-        console.log('Received values:', values);
+        console.log("Received values:", values);
     };
 
     return (
         <div>
-            <Row justify="space-between" align="middle" className='h-max'>
+            <Row justify="space-between" align="middle" className="h-max">
                 <Col span={24} md={12}>
-                    <Typography.Title level={2} style={{color: 'black'}}>{t('PROJECTS.CREATEPROJECT')}</Typography.Title>
-                </Col>
-                <Col span={24} md={12} style={{textAlign: 'right'}}>
-                    <Button onClick={() => navigate('')} type="primary"
-                            style={{
-                                backgroundColor: '#000',
-                                borderColor: '#000',
-                                color: '#fff'
-                            }}>
-                        {t('PROJECTS.SAVEPROJECT')}</Button>
+                    <Typography.Title level={2} style={{ color: "black" }}>
+                        {t("PROJECTS.CREATEPROJECT")}
+                    </Typography.Title>
                 </Col>
             </Row>
             <Breadcrumb className="my-4 text-[#101011] cursor-pointer">
-                <Breadcrumb.Item onClick={() => {
-                    navigate("/");
-                }}
-                >{t('PROJECTS.PROJECT5')}</Breadcrumb.Item>
-                <Breadcrumb.Item onClick={() => {
-                    navigate("/project");
-                }}
-                >{t('PROJECTS.CREATE1')}</Breadcrumb.Item>
+                <Breadcrumb.Item
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                >
+                    {t("PROJECTS.PROJECT5")}
+                </Breadcrumb.Item>
+                <Breadcrumb.Item
+                    onClick={() => {
+                        navigate("/project");
+                    }}
+                >
+                    {t("PROJECTS.CREATE1")}
+                </Breadcrumb.Item>
             </Breadcrumb>
-            <Divider className="divider-dark"/>
-            <Form
-                name="product_form"
-                layout="vertical"
-                onFinish={onFinish}
-            >
+            <Divider className="divider-dark" />
+            <Form name="product_form" layout="vertical" onFinish={onFinish}>
+                <Row justify="end">
+
+                    <Button
+                        onClick={() => navigate("/project")}
+                        type="primary"
+                        htmlType="submit"
+                        style={{
+                            backgroundColor: "#000",
+                            borderColor: "#000",
+                            color: "#fff",
+                        }}
+                    >
+                        {t("PROJECTS.SAVEPROJECT")}
+                    </Button>
+                </Row>
                 <Row gutter={16}>
                     <Col span={12} xs={24} sm={12} md={24} lg={12} xl={12}>
                         <Form.Item
                             name="productName"
-                            label={t('PROJECTS.PRODUCT')}
-                            rules={[{required: true, message: 'Please enter product name!'}]}
+                            label={t("PROJECTS.PRODUCT")}
+                            rules={[
+                                { required: true, message: "Please enter product name!" },
+                            ]}
                         >
-                            <Input placeholder={t('PROJECTS.PRODUCT1')}/>
+                            <Input placeholder={t("PROJECTS.PRODUCT1")} />
                         </Form.Item>
-                        <Form.Item className='mt-3'
+                        <Form.Item
+                            className="mt-3"
                             name="productTechnical"
-                            label={t('PROJECTS.TECHNICAL')}
-                            rules={[{required: true, message: 'Please select product technical!'}]}
+                            label={t("PROJECTS.TECHNICAL")}
+                            rules={[
+                                { required: true, message: "Please select product technical!" },
+                            ]}
                         >
-                            <Select placeholder={t('PROJECTS.TECHNICAL1')}>
-                                <Option value="technical1">{t('PROJECTS.SELECT4')}</Option>
-                                <Option value="technical2">{t('PROJECTS.SELECT5')}</Option>
-                                <Option value="technical3">{t('PROJECTS.SELECT6')}</Option>
+                            <Select placeholder={t("PROJECTS.TECHNICAL1")}>
+                                <Option value="technical1">{t("PROJECTS.SELECT4")}</Option>
+                                <Option value="technical2">{t("PROJECTS.SELECT5")}</Option>
+                                <Option value="technical3">{t("PROJECTS.SELECT6")}</Option>
                             </Select>
                         </Form.Item>
-                        <Form.Item className='mt-3'
-                            name="productManager"
-                            label={t('PROJECTS.MANAGER')}
-                            rules={[{required: true, message: 'Please select product manager!'}]}
+                        <Form.Item
+                            className="mt-3"
+                            name="productDescription"
+                            label={t("PROJECTS.DESCRIPTION")}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter product description!",
+                                },
+                            ]}
                         >
-                            <Select placeholder={t('PROJECTS.MANAGER1')}>
-                                <Option value="manager1">{t('PROJECTS.SELECT1')}</Option>
-                                <Option value="manager2">{t('PROJECTS.SELECT2')}</Option>
-                                <Option value="manager3">{t('PROJECTS.SELECT3')}</Option>
-                            </Select>
+                            <Input.TextArea placeholder={t("PROJECTS.PRODUCT2")} />
                         </Form.Item>
                     </Col>
                     <Col span={12} xs={24} sm={12} md={24} lg={12} xl={12}>
                         <Form.Item
+                            className="mt-3 md:mt-0"
                             name="projectTime"
-                            label={t('PROJECTS.TIME')}
-                            rules={[{required: true, message: 'Please enter project time!'}]}
+                            label={t("PROJECTS.TIME")}
+                            rules={[
+                                { required: true, message: "Please enter project time!" },
+                            ]}
                         >
                             <Col span={24} xs={24} sm={24} md={24} lg={24} xl={24}>
-                                <DatePicker.RangePicker style={{width: '100%'}}/>
+                                <DatePicker.RangePicker style={{ width: "100%" }} />
                             </Col>
                         </Form.Item>
-
-                        <Form.Item className='mt-3'
-                            name="productDescription"
-                            label={t('PROJECTS.DESCRIPTION')}
-                            rules={[{required: true, message: 'Please enter product description!'}]}
+                        <Form.Item
+                            className="mt-3"
+                            name="productManager"
+                            label={t("PROJECTS.MANAGER")}
+                            rules={[
+                                { required: true, message: "Please select product manager!" },
+                            ]}
                         >
-                            <Input.TextArea placeholder={t('PROJECTS.PRODUCT2')}/>
+                            <Select placeholder={t("PROJECTS.MANAGER1")}>
+                                <Option value="manager1">{t("PROJECTS.SELECT1")}</Option>
+                                <Option value="manager2">{t("PROJECTS.SELECT2")}</Option>
+                                <Option value="manager3">{t("PROJECTS.SELECT3")}</Option>
+                            </Select>
                         </Form.Item>
                     </Col>
                 </Row>
                 <Row gutter={16}>
                     <Col span={12} xs={24} sm={12} md={24} lg={12} xl={12}>
-                        <Form.Item className='mt-3'
+                        <Form.Item
+                            className="mt-3"
                             name="productMember"
-                            label={t('PROJECTS.MEMBER')}
-                            rules={[{required: true, message: 'Please select product member!'}]}
+                            label={t("PROJECTS.MEMBER")}
+                            rules={[
+                                { required: true, message: "Please select product member!" },
+                            ]}
                         >
-                            <Select mode="multiple" placeholder={t('PROJECTS.MEMBER1')}>
-                                <Option value="member1">{t('PROJECTS.SELECT7')}</Option>
-                                <Option value="member2">{t('PROJECTS.SELECT8')}</Option>
-                                <Option value="member3">{t('PROJECTS.SELECT9')}</Option>
+                            <Select mode="multiple" placeholder={t("PROJECTS.MEMBER1")}>
+                                <Option value="member1">{t("PROJECTS.SELECT7")}</Option>
+                                <Option value="member2">{t("PROJECTS.SELECT8")}</Option>
+                                <Option value="member3">{t("PROJECTS.SELECT9")}</Option>
                             </Select>
                         </Form.Item>
                     </Col>
                     <Col span={12} xs={24} sm={12} md={24} lg={12} xl={12}>
-                        <Form.Item className='mt-3'
+                        <Form.Item
+                            className="mt-3"
                             name="productRole"
-                            label={t('PROJECTS.ROLE')}
-                            rules={[{required: true, message: 'Please select product role!'}]}
+                            label={t("PROJECTS.ROLE")}
+                            rules={[
+                                { required: true, message: "Please select product role!" },
+                            ]}
                         >
-                            <Select mode="multiple" placeholder={t('PROJECTS.ROLE1')}>
+                            <Select mode="multiple" placeholder={t("PROJECTS.ROLE1")}>
                                 <Option value="role1">Role 1</Option>
                                 <Option value="role2">Role 2</Option>
                                 <Option value="role3">Role 3</Option>
@@ -128,10 +171,18 @@ const CreateProject = () => {
                 </Row>
                 <Row justify="start">
                     <Col>
-                        <Form.Item className='mt-3'>
-                            <Button type="primary" style={{backgroundColor: '#000', borderColor: '#000', color: '#fff'}}
-                                    icon={<PlusOutlined/>} htmlType="submit">
-                                {t('PROJECTS.CREATE')}
+                        <Form.Item className="mt-3">
+                            <Button
+                                type="primary"
+                                style={{
+                                    backgroundColor: "#000",
+                                    borderColor: "#000",
+                                    color: "#fff",
+                                }}
+                                icon={<PlusOutlined />}
+                                htmlType="submit"
+                            >
+                                {t("PROJECTS.CREATE")}
                             </Button>
                         </Form.Item>
                     </Col>
