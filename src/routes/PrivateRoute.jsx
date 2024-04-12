96b4baf3143db67edcaa647218b3a8d8df8fa769
VoVanKhoa <vankhoa.work@gmail.com>
2024-04-12 08:39:56 +0700

    feat/emptw19/create-project-management

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,6 +2,8 @@ import Dashboard from '../pages/Dashboard/Dashboard';
 import EmployList from '../pages/Employees/EmployeeList/EmployList';
 import ProjectList from '../pages/Project/ProjectList/ProjectList';
 import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
+import CreateProject from "../pages/Project/CreateProject/CreateProject.jsx";
+import UpdateEmployee from "../pages/Employees/UpdateEmployee/UpdateEmployee.jsx";
 
 const PrivateRoute = [
     {
@@ -19,6 +21,10 @@ const PrivateRoute = [
                         path: '',
                         element: <ProjectList />,
                     },
+                    {
+                        path: 'create',
+                        element: <CreateProject />,
+                    },
                 ],
             },
             {
@@ -27,6 +33,10 @@ const PrivateRoute = [
                     {
                         path: '',
                         element: <EmployList />,
+                    },
+                    {
+                        path: 'update',
+                        element: <UpdateEmployee />,
                     },
                 ],
             },
